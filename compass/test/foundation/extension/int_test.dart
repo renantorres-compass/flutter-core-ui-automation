fd5c50d697f5241a2822f1491d0fdc12d49982e9
Renan Campos Torres <106977020+renantorres-compass@users.noreply.github.com>
2022-06-22 15:44:38 -0300

    Create int_test.dart

---
+++ compass/test/foundation/extension/int_test.dart
@@ -0,0 +1,17 @@
+import 'package:compass/foundation/extension/int.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test("Get a value and return in a money format", () {
+    int value = 1234;
+
+    int value1 = 10444;
+    int value2 = 0;
+
+    expect(value.moneyFormat, "R\$ 1.234,00");
+
+    expect(value1.moneyFormat, "R\$ 10.444,00");
+
+    expect(value2.moneyFormat, "R\$ 0,00");
+  });
+}
