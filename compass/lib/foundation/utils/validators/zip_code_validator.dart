3ad055d2063e0dba6a7a8882940454046df338be
Renan Campos Torres <106977020+renantorres-compass@users.noreply.github.com>
2022-06-24 11:47:19 -0300

    Create ZipCode Validator and create a test

---
+++ compass/lib/foundation/utils/validators/zip_code_validator.dart
@@ -0,0 +1,16 @@
+class ZipCodeValidator {
+  final String errorMessage;
+
+  ZipCodeValidator({required this.errorMessage});
+
+  String? validateZipCode(String? zipCode) {
+    if (zipCode == null ||
+        zipCode.isEmpty ||
+        zipCode.trim().replaceAll(RegExp(r'[^\w\s]+'), "").length < 8 ||
+        zipCode.trim().replaceAll(RegExp(r'[^\w\s]+'), "").length > 8) {
+      return errorMessage;
+    } else {
+      return null;
+    }
+  }
+}
---
+++ compass/test/foundation/utils/validators/zip_code_validator_test.dart
@@ -0,0 +1,46 @@
+import 'package:compass/foundation/utils/validators/zip_code_validator.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('Return error message if Zip Code is null', () {
+    String errorMessage = "CEP inválido";
+    expect(ZipCodeValidator(errorMessage: errorMessage).validateZipCode(null),
+        "CEP inválido");
+  });
+
+  test('Return error message if Zip Code is empty', () {
+    String errorMessage = "CEP inválido";
+    expect(ZipCodeValidator(errorMessage: errorMessage).validateZipCode(""),
+        "CEP inválido");
+  });
+
+  test(
+      '''Return error message if Zip Code length's is lower or greater than 9''',
+      () {
+    String errorMessage = "CEP inválido";
+    expect(
+        ZipCodeValidator(errorMessage: errorMessage).validateZipCode("7222222"),
+        "CEP inválido");
+
+    expect(
+        ZipCodeValidator(errorMessage: errorMessage).validateZipCode("72222-2"),
+        "CEP inválido");
+
+    expect(
+        ZipCodeValidator(errorMessage: errorMessage).validateZipCode("72222-2"),
+        "CEP inválido");
+  });
+
+  test('Return null if Zip Code is valid', () {
+    String errorMessage = "CEP inválido";
+    expect(
+        ZipCodeValidator(errorMessage: errorMessage)
+            .validateZipCode("72222222"),
+        null);
+
+    expect(
+        ZipCodeValidator(errorMessage: errorMessage)
+            .validateZipCode("72222-222"),
+        null);
+  });
+}
